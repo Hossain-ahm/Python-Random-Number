7505b6be51b73f3cb02831f8dea86f452f7d68f5
Hossain Ahmed <hossainahmed2002@gmail.com>
2023-06-16 21:23:28 +0100

    made the app give a singular random number

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import random
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 
 def random_Numbers(len,min,max):
@@ -10,12 +11,11 @@ def random_Numbers(len,min,max):
 
 
 def random_Number():
-    print ("Give me a min and a max value and I will give you a random number inbetween")
 
-    min = input("min: ")
-    max = input("max: ")
+    min = float(min1.get())
+    max = float(max1.get())
 
-    return random.randint(int(min),int(max))
+    randomNumber.set(random.randint(int(min),int(max)))
 
 def random_SetOfNumber():
     print("Give me how much random number would you like:")
@@ -28,3 +28,35 @@ def random_SetOfNumber():
     numbers = random_Numbers(len,min,max)
     return numbers
 
+
+root = Tk()
+#the titlte of the app
+root.title("Random numbers")
+
+#the main frame all the widgets will be placed inside
+topframe = ttk.Frame(root, padding=(3,3,12,12))
+topframe.grid(column=0, row=0)
+
+#field to put the min input for singular random number
+min1 = StringVar()
+min1_entry = ttk.Entry(topframe, textvariable=min1)
+ttk.Label(topframe, text="minimum number").grid(column=0, row=1)
+min1_entry.grid(column=1, row=1)
+
+
+#field to put the max input for singular random number
+max1 = StringVar()
+max1_entry = ttk.Entry(topframe, textvariable=max1)
+ttk.Label(topframe, text="maximum number").grid(column=2, row=1)
+max1_entry.grid(column=3, row=1)
+
+
+ttk.Label(topframe, text="Random number").grid(column=0, row=0)
+#A button when preseed will give the random number
+ttk.Button(topframe, text="Calculate",command=random_Number).grid(column=0, row=2, sticky=W)
+
+randomNumber = StringVar()
+ttk.Label(topframe, textvariable=randomNumber).grid(column=2, row=2, sticky=(W, E))
+
+
+root.mainloop()
