9edd05634a7cbc1999de84b701729ccf476e165f
Hossain Ahmed <hossainahmed2002@gmail.com>
2023-06-16 17:33:12 +0100

    Made so the fumctions return values instead of printing them

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
-def random_SingleNumber(min,max):
-        print (random.randint(int(min),int(max)))
+import tkinter as tk
+
 
 def random_Numbers(len,min,max):
     numbers = []
@@ -15,10 +15,7 @@ def random_Number():
     min = input("min: ")
     max = input("max: ")
 
-    random_SingleNumber(min,max)
-
-
-
+    return random.randint(int(min),int(max))
 
 def random_SetOfNumber():
     print("Give me how much random number would you like:")
@@ -29,8 +26,5 @@ def random_SetOfNumber():
     max = input("max: ")
     
     numbers = random_Numbers(len,min,max)
-    for i in numbers:
-         print(str(i))
-
-random_SetOfNumber()
+    return numbers
 
